b235be050e8552bfc60696c73b59e1d5ddbea690
oneilld136 <oneilld136@gmail.com>
2019-03-20 14:35:34 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -40,7 +40,7 @@ shipping_manifest ["gun powder"]=4
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3
-                  "muskets"=>2
-                  "gun powder"=>4
+                  "muskets"=> 2
+                  "gun powder"=> 4
                  }
 end
